94f56f4e1c38b7438a1dff5a0a6b2e7fb150b9cf
lisapowell1991 <lisapowell46@gmail.com>
2023-11-24 13:35:37 -0800

    execute and pkill process

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,10 @@
+# Using Puppet, create a manifest that kills a process named killmenow.
+
+# Requirements:
+# Must use the exec Puppet resource
+# Must use pkill
+
+exec { 'killmenow':
+  command     => '/usr/bin/pkill -9 killmenow',
+  refreshonly => true,
+}
