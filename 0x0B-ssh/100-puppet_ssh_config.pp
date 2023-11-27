ff9eccbe2b2ca07721fbbc61646bc795652185d8
lisapowell1991 <lisapowell46@gmail.com>
2023-11-26 21:53:30 -0800

    added ensure => present

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -7,11 +7,13 @@
 include stdlib
 
 file_line { 'Turn off passwd auth':
-    path => '/etc/ssh/sshd_config',
-    line => 'PasswordAuthentication no',
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-    path => '/etc/ssh/ssh_config',
-    line => 'IdentityFile ~/.ssh/school',
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => 'IdentityFile ~/.ssh/school',
 }
