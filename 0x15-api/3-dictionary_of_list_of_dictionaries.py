31e2e067387ee4d79a2075c8a60d9fffb71366f4
lisapowell1991 <lisapowell46@gmail.com>
2024-01-02 13:56:43 -0800

    looped through al users

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,45 +11,48 @@ from sys import argv
 
 if __name__ == "__main__":
     base_url = 'https://jsonplaceholder.typicode.com'
-    employee_id = int(argv[1])
+    # employee_id = int(argv[1])
 
     # Create a dictionary to store tasks for each user
     all_tasks = {}
 
     # User details
-    user_response = get(f"{base_url}/users/{employee_id}")
-    user_data = user_response.json()
+    users_response = get(f"{base_url}/users")
+    users_data = users_response.json()
 
-    # User's todos
-    todos_response = get(f"{base_url}/todos?userId={employee_id}")
-    todos_data = todos_response.json()
+    for user in users_data:
+        employee_id = user['id']
 
-    # Calculate todo list progress
-    total_tasks = len(todos_data)
-    completed_tasks = sum(1 for todo in todos_data if todo['completed'])
+        # User's todos
+        todos_response = get(f"{base_url}/todos?userId={employee_id}")
+        todos_data = todos_response.json()
 
-    # Create a list to store tasks for the current user
-    user_tasks = []
+        # Calculate todo list progress
+        total_tasks = len(todos_data)
+        completed_tasks = sum(1 for todo in todos_data if todo['completed'])
 
-    # Populate user_tasks list with task details
-    for todo in todos_data:
-        task_details = {
-            "username": user_data['username'],
-            "task": todo['title'],
-            "completed": todo['completed']
-        }
-        user_tasks.append(task_details)
+        # Create a list to store tasks for the current user
+        user_tasks = []
 
-    # Add user_tasks list to the all_tasks dictionary with the key as USER_ID
-    all_tasks[str(employee_id)] = user_tasks
+        # Populate user_tasks list with task details
+        for todo in todos_data:
+            task_details = {
+                "username": user['username'],
+                "task": todo['title'],
+                "completed": todo['completed']
+            }
+            user_tasks.append(task_details)
 
-    # Write the all_tasks dictionary to a JSON file
-    with open('todo_all_employees.json', 'w') as json_file:
-        json.dump(all_tasks, json_file, indent=2)
+        # Add user_tasks list to the all_tasks dict with the key as USER_ID
+        all_tasks[str(employee_id)] = user_tasks
 
-    # Display progress info
-    print(f"Employee {user_data['name']} is done with tasks"
-          f"({completed_tasks}/{total_tasks}):")
+        # Write the all_tasks dictionary to a JSON file
+        with open('todo_all_employees.json', 'w') as json_file:
+            json.dump(all_tasks, json_file, indent=2)
+
+        # Display progress info
+        print(f"Employee {user['name']} is done with tasks"
+              f"({completed_tasks}/{total_tasks}):")
 
     # Display completed tasks titles
     for todo in todos_data:
