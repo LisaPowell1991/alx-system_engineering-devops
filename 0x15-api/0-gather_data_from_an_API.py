02d58ac8baf3046c34a7df948ebcefd35b8fc0e0
lisapowell1991 <lisapowell46@gmail.com>
2024-01-02 07:17:29 -0800

    updated file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,31 +5,49 @@ Using this REST API(https://jsonplaceholder.typicode.com),
 for a given employee ID, returns information about
 his/her TODO list progress.
 """
-from requests import get
-from sys import argv
+import sys
+import requests
 
 
-if __name__ == "__main__":
+def get_employee_todo_progress(employee_id):
+    """
+    Get and display info about an employee's
+    TODO list progress.
+
+    Parameters:
+    - employee_id (int): The ID of the employee
+    whose progress needs to be checked.
+    """
     base_url = 'https://jsonplaceholder.typicode.com'
-    employee_id = int(argv[1])
 
     # User details
-    user_response = get(f"{base_url}/users/{employee_id}")
+    user_response = requests.get(f"{base_url}/users/{employee_id}")
     user_data = user_response.json()
 
     # User's todos
-    todos_response = get(f"{base_url}/todos?userId={employee_id}")
+    todos_response = requests.get(f"{base_url}/todos?userId={employee_id}")
     todos_data = todos_response.json()
 
     # Calculate todo list progress
     total_tasks = len(todos_data)
-    completed_tasks = sum(1 for todo in todos_data if todo['completed'])
+    completed_tasks = sum(1 for todo in todos_data if todo.get('completed'))
 
     # Display progress info
     print(f"Employee {user_data['name']} is done with tasks "
           f"({completed_tasks}/{total_tasks}):")
+    print(f"{user_data['name']}:
+            {completed_tasks} tasks completed out of {total_tasks}")
 
     # Display completed tasks titles
     for todo in todos_data:
-        if todo['completed']:
+        if todo.get('completed'):
             print(f"\t{todo['title']}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
