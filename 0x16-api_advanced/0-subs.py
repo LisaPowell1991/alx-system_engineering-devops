5811481c1ad414b132a6e233d619d4a47b2357e8
lisapowell1991 <lisapowell46@gmail.com>
2024-01-16 02:08:45 -0800

    Get the number of subscribers for a given subreddit.

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+
+"""
+A module that contains a function "number_of_subscribers".
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
+
+    Args:
+    subreddit: The name of the subreddit.
+
+    Returns:
+    int: The number of subscribers. If the subreddit is invalid,
+    return 0.
+    """
+
+    # Set custom User-Agent
+    headers = {'User-Agent': 'MyRedditApp/1.0 (by /u/jane_doe)'}
+
+    # Construct API URL
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    # Make request
+    response = requests.get(url, headers=headers)
+
+    # Check if  response is successful/invalid
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
