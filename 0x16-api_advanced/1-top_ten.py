aa6d0a26ac71393b913e751b46901de2488a1be5
lisapowell1991 <lisapowell46@gmail.com>
2024-01-16 06:44:54 -0800

    changed user agent

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -22,7 +22,7 @@ def top_ten(subreddit):
     Or None if the subreddit is invalid
     """
     # Set custom User-Agent
-    headers = {'User-Agent': '0x16.api.advanced:v1.0.0 (by /u/jane_doe)'}
+    headers = {'User-Agent': 'alx-lisa-api'}
 
     # Construct API URL for hot posts
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
