fe5a669b114783860ae85dc05656c1d20b852337
lisapowell1991 <lisapowell46@gmail.com>
2024-01-16 04:46:49 -0800

    queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,43 @@
 #!/usr/bin/python3
+
+
+"""
+A module that contains a function top_ten.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the
+    titles of the first 10 hot posts
+    for a given subreddit.
+
+    Args:
+    subreddit (str): The name of the subreddit.
+
+    Returns:
+    Prints the titles  of the first 10 hot posts
+    Or None if the subreddit is invalid
+    """
+    # Set custom User-Agent
+    headers = {'User-Agent': '0x16.api.advanced:v1.0.0 (by /u/jane_doe)'}
+
+    # Construct API URL for hot posts
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+
+    # Make request
+    response = requests.get(url, headers=headers)
+
+    # Check if response is successful/invalid
+    if response.status_code == 200:
+        posts_data = response.json().get('data', {}).get('children', [])
+
+        # Check if subreddit exists
+        for post in post_data:
+            print(post['data']['title'])
+    elif response.status_code == 404:
+        print('None')
+    else:
+        print('None')
