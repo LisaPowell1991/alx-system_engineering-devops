50d626ace482f5f9fc228c0d0b116af8eb779e19
lisapowell1991 <lisapowell46@gmail.com>
2024-01-17 11:34:29 +0000

    puppet-lint style

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
+# Fix WordPress Configuration with sed
 # Using strace, find out why Apache is returning a 500 error.
 
-exec { "fix-wordpress":
-    command => "sed -i s/phpp/php/g /var/www/html/wp-settings.php",
-    path    => "/usr/local/bin/:/bin/"
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
 }
